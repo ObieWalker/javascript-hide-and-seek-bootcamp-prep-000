35f1e47c0e361653963d805bae3b61ae3a63ba30
Obi <obinnawalker@gmail.com>
2017-11-07 10:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ return increase
 }
 
 function deepestChild(){
-  let node = document.getElementById('app').querySelectorAll('div#grand-node div div div div ')
+  let node = document.getElementById('app').querySelector('#grand-node')
+  let nextNode = node.children[0]
 
+  while (nextNode){
+    node = nextNode
+    nextNode = node.children[0]
+  }
   return node
 }
